81c67877d6f4777ba0ab684dd8989eed390b924c
Aleksandra Azarova <alekssaffl@yandex.ru>
2022-01-26 00:17:26 +0300

    refactor: add useCallback

--- client/components/pages/TodoPage.tsx
+++ client/components/pages/TodoPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { tasks } from '../../taskManager/Tasks';
 import { Todo } from '../templates/Todo/TodoTemplate';
@@ -7,19 +7,28 @@ import { ITask } from '../../taskManager/models';
 export const TodoPage: React.FC = () => {
   const [allTasks, setTasks] = useState(tasks);
 
-  const addTask = (newTask: ITask) => {
-    setTasks([...allTasks, newTask]);
-  };
+  const addTask = useCallback(
+    (newTask: ITask) => {
+      setTasks([...allTasks, newTask]);
+    },
+    [allTasks],
+  );
 
-  const deleteTask = (taskId: number) => {
-    const newTasks = allTasks.filter((task) => task.id !== taskId);
-    setTasks(newTasks);
-  };
+  const deleteTask = useCallback(
+    (taskId: number) => {
+      const newTasks = allTasks.filter((task) => task.id !== taskId);
+      setTasks(newTasks);
+    },
+    [allTasks],
+  );
 
-  const updateTask = (newTaskData: ITask) => {
-    const newTasks = allTasks.map((task) => (task.id === newTaskData.id ? newTaskData : task));
-    setTasks(newTasks);
-  };
+  const updateTask = useCallback(
+    (newTaskData: ITask) => {
+      const newTasks = allTasks.map((task) => (task.id === newTaskData.id ? newTaskData : task));
+      setTasks(newTasks);
+    },
+    [allTasks],
+  );
 
   return (
     <Todo tasks={allTasks} addNewTask={addTask} updateTask={updateTask} deleteTask={deleteTask} />
